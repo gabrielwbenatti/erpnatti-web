f680f45c6f822f302b681e75318c212614f38ea1
Gabriel Wisky Benatti <70673950+gabrielwbenatti@users.noreply.github.com>
2024-06-05 07:27:09 -0300

    Initial Commit

---
+++ src/components/Modal.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="modal">
+        fuhefiuehiuf heifuheif
+    </div>
+</template>
+
+<style>
+.modal {
+    /* display: none; */
+    /* position: fixed; */
+}
+</style>
