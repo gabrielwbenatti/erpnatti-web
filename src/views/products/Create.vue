f72244bb955e9b0a28f2daa201c277bf24fcfd7c
Gabriel Wisky Benatti <70673950+gabrielwbenatti@users.noreply.github.com>
2024-06-08 13:00:26 -0300

    tratativa para nao gravar ref. duplicada

--- src/views/products/Create.vue
+++ src/views/products/Create.vue
@@ -1,16 +1,31 @@
 <script setup>
-import router from '@/router';
-import { ref } from 'vue';
+import router from "@/router";
+import { ref } from "vue";
 
-const reference = ref("")
-const name = ref("")
-const nameAlias = ref("")
-const status = ref(true)
-const toSell = ref(true)
-const toBuy = ref(true)
+const reference = ref("");
+const name = ref("");
+const nameAlias = ref("");
+const status = ref(true);
+const toSell = ref(true);
+const toBuy = ref(true);
+
+async function referenceAlreadyExists(reference) {
+    const url = new URL("http://localhost:9000/products");
+    console.log(`ref: ${reference.trim()}`);
+    url.searchParams.append("reference", reference.trim());
+    const response = await fetch(url);
+    if (response) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 async function save() {
-    await fetch('http://localhost:9000/products', {
+    if (referenceAlreadyExists(reference.value)) return;
+
+    const url = new URL("http://localhost:9000/products");
+    await fetch(url, {
         method: "POST",
         body: JSON.stringify({
             reference: reference.value,
@@ -18,16 +33,16 @@ async function save() {
             nameAlias: nameAlias.value,
             status: status.value,
             toSell: toSell.value,
-            toBuy: toBuy.value
+            toBuy: toBuy.value,
         }),
         headers: {
-            "Content-type": "application/json; charset=UTF-8"
-        }
-    }).then(res => {
+            "Content-type": "application/json; charset=UTF-8",
+        },
+    }).then((res) => {
         if (res.status === 201) {
-            router.back()
+            router.back();
         }
-    })
+    });
 }
 </script>
 
@@ -35,39 +50,80 @@ async function save() {
     <main>
         <div>
             <h2>Novo Produto</h2>
-            <br>
+            <br />
+        </div>
+
+        <div class="labeled__input">
+            <label for="reference" class="labeled__input-label"
+                >Referência</label
+            >
+            <input
+                type="text"
+                name="reference"
+                id="reference"
+                autocomplete="off"
+                v-model="reference"
+                class="labeled__input-input"
+            />
+        </div>
+
+        <div class="labeled__input">
+            <label for="name" class="labeled__input-label">Nome</label>
+            <input
+                type="text"
+                name="name"
+                id="name"
+                autocomplete="off"
+                v-model="name"
+                class="labeled__input-input"
+            />
+        </div>
+
+        <div class="labeled__input">
+            <label for="nameAlias" class="labeled__input-label"
+                >Nome Apelido</label
+            >
+            <input
+                type="text"
+                name="nameAlias"
+                id="nameAlias"
+                autocomplete="off"
+                v-model="nameAlias"
+                class="labeled__input-input"
+            />
         </div>
 
         <div>
-            <label for="reference">Referência</label>
-            <input type="text" name="reference" id="reference" autocomplete="off" v-model="reference"
-                class="input__text">
-        </div>
-
-        <div>
-            <label for="name">Nome</label>
-            <input type="text" name="name" id="name" autocomplete="off" v-model="name" class="input__text">
-        </div>
-
-        <div>
-            <label for="nameAlias">Nome Apelido</label>
-            <input type="text" name="nameAlias" id="nameAlias" autocomplete="off" v-model="nameAlias"
-                class="input__text">
-        </div>
-
-        <div>
+            <input
+                type="checkbox"
+                name="status"
+                id="status"
+                v-model="status"
+                class="checkbox"
+            />
             <label for="status">Status</label>
-            <input type="checkbox" name="status" id="status" v-model="status">
         </div>
 
-        <div style="display: block;">
+        <div style="display: block">
+            <input
+                type="checkbox"
+                name="toSell"
+                id="toSell"
+                v-model="toSell"
+                class="checkbox"
+            />
             <label for="toSell">Para Vender</label>
-            <input type="checkbox" name="toSell" id="toSell" v-model="toSell">
         </div>
 
         <div>
+            <input
+                type="checkbox"
+                name="toBuy"
+                id="toBuy"
+                v-model="toBuy"
+                class="checkbox"
+            />
             <label for="toBuy">Para Comprar</label>
-            <input type="checkbox" name="toBuy" id="toBuy" v-model="toBuy">
         </div>
 
         <button type="button" @click="save">Salvar</button>
@@ -76,8 +132,22 @@ async function save() {
 </template>
 
 <style scoped>
-.input__text {
-    padding: 8px;
-    width: 60vw;
+.labeled__input {
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 1em;
+
+    .labeled__input-input {
+        padding: 8px;
+        width: 60vw;
+    }
+
+    .labeled__input-label {
+        margin-bottom: 0.3em;
+    }
+}
+
+.checkbox {
+    margin-right: 0.3em;
 }
 </style>
