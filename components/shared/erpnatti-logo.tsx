a2c666cbcbbec707dad0772d94578f40c164e131
Infonacci Gabriel <gabriel@infonacci.com.br>
2025-01-22 14:40:16 -0300

    criação de novos componentes e paginas

---
+++ components/shared/erpnatti-logo.tsx
@@ -0,0 +1,10 @@
+function ErpNattiLogo() {
+  return (
+    <div className="flex text-2xl font-bold">
+      <span className="text-black">erp</span>
+      <span className="text-primary-600">natti</span>
+    </div>
+  );
+}
+
+export default ErpNattiLogo;
